da7a24de95dcd0aa3b034f643f786c7de5970bc7
Ben Riegel <ben.riegel@gmail.com>
2018-01-10 20:48:07 -0800

    Assignment 26: completed

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,15 @@ class Post < ApplicationRecord
   has_many :comments, dependent: :destroy
 
   default_scope { order('created_at DESC') }
+
+  def self.ordered_by_title
+    order('title ASC')
+  end
+
+  def self.ordered_by_reverse_created_at
+    order ('created_at ASC')
+  end
+
   validates :title, length: { minimum: 5 }, presence: true
   validates :body, length: { minimum: 20 }, presence: true
   validates :topic, presence: true
